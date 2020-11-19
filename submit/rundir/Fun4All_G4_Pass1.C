d2ce912ea17f5c86c018926129f79a5895a9dde9
sPHENIX Production Account <pinkenburg@bnl.gov>
2020-11-19 11:06:03 -0500

    add preliminary submission scripts (until PanDa comes online)

---
+++ submit/rundir/Fun4All_G4_Pass1.C
@@ -0,0 +1,289 @@
+#ifndef MACRO_FUN4ALLG4SPHENIX_C
+#define MACRO_FUN4ALLG4SPHENIX_C
+
+#include <GlobalVariables.C>
+
+#include <DisplayOn.C>
+#include <G4Setup_sPHENIX.C>
+#include <G4_Bbc.C>
+#include <G4_CaloTrigger.C>
+#include <G4_DSTReader.C>
+#include <G4_Global.C>
+#include <G4_HIJetReco.C>
+#include <G4_Input.C>
+#include <G4_Jets.C>
+#include <G4_ParticleFlow.C>
+#include <G4_Production.C>
+#include <G4_TopoClusterReco.C>
+#include <G4_Tracking.C>
+#include <G4_User.C>
+
+#include <ffamodules/SyncReco.h>
+
+#include <fun4all/Fun4AllDstOutputManager.h>
+#include <fun4all/Fun4AllOutputManager.h>
+#include <fun4all/Fun4AllServer.h>
+
+#include <phool/PHRandomSeed.h>
+#include <phool/recoConsts.h>
+
+R__LOAD_LIBRARY(libfun4all.so)
+R__LOAD_LIBRARY(libffamodules.so)
+
+// For HepMC Hijing
+// try inputFile = /sphenix/sim/sim01/sphnxpro/sHijing_HepMC/sHijing_0-12fm.dat
+
+int Fun4All_G4_Pass1(
+    const int nEvents = 1,
+    const string &inputFile = "https://www.phenix.bnl.gov/WWW/publish/phnxbld/sPHENIX/files/sPHENIX_G4Hits_sHijing_9-11fm_00000_00010.root",
+    const string &outputFile = "G4sPHENIX.root",
+    const string &embed_input_file = "https://www.phenix.bnl.gov/WWW/publish/phnxbld/sPHENIX/files/sPHENIX_G4Hits_sHijing_9-11fm_00000_00010.root",
+    const int skip = 0,
+    const string &outdir = ".")
+{
+  Fun4AllServer *se = Fun4AllServer::instance();
+  se->Verbosity(0);
+
+  //Opt to print all random seed used for debugging reproducibility. Comment out to reduce stdout prints.
+  PHRandomSeed::Verbosity(1);
+
+  // just if we set some flags somewhere in this macro
+  recoConsts *rc = recoConsts::instance();
+  // By default every random number generator uses
+  // PHRandomSeed() which reads /dev/urandom to get its seed
+  // if the RANDOMSEED flag is set its value is taken as seed
+  // You can either set this to a random value using PHRandomSeed()
+  // which will make all seeds identical (not sure what the point of
+  // this would be:
+  //  rc->set_IntFlag("RANDOMSEED",PHRandomSeed());
+  // or set it to a fixed value so you can debug your code
+  //  rc->set_IntFlag("RANDOMSEED", 12345);
+
+  //===============
+  // Input options
+  //===============
+  // verbosity setting (applies to all input managers)
+  Input::VERBOSITY = 1;
+  Input::HEPMC = true;
+  
+  INPUTHEPMC::filename = inputFile;
+  INPUTHEPMC::FLOW = true;
+//  INPUTHEPMC::FLOW_VERBOSITY = 3;
+//  INPUTHEPMC::FERMIMOTION = true;
+
+  // Event pile up simulation with collision rate in Hz MB collisions.
+  //Input::PILEUPRATE = 100e3;
+
+  //-----------------
+  // Initialize the selected Input/Event generation
+  //-----------------
+  // This creates the input generator(s)
+  InputInit();
+
+  //--------------
+  // Set Input Manager specific options
+  //--------------
+  // can only be set after InputInit() is called
+
+  if (Input::HEPMC)
+  {
+    INPUTMANAGER::HepMCInputManager->set_vertex_distribution_width(100e-4, 100e-4, 8, 0);  //optional collision smear in space, time
+                                                                                           //    INPUTMANAGER::HepMCInputManager->set_vertex_distribution_mean(0,0,0,0);//optional collision central position shift in space, time
+    // //optional choice of vertex distribution function in space, time
+    INPUTMANAGER::HepMCInputManager->set_vertex_distribution_function(PHHepMCGenHelper::Gaus, PHHepMCGenHelper::Gaus, PHHepMCGenHelper::Gaus, PHHepMCGenHelper::Gaus);
+    //! embedding ID for the event
+    //! positive ID is the embedded event of interest, e.g. jetty event from pythia
+    //! negative IDs are backgrounds, .e.g out of time pile up collisions
+    //! Usually, ID = 0 means the primary Au+Au collision background
+    //INPUTMANAGER::HepMCInputManager->set_embedding_id(Input::EmbedID);
+    if (Input::PILEUPRATE > 0)
+    {
+      // Copy vertex settings from foreground hepmc input
+      INPUTMANAGER::HepMCPileupInputManager->CopyHelperSettings(INPUTMANAGER::HepMCInputManager);
+      // and then modify the ones you want to be different
+      // INPUTMANAGER::HepMCPileupInputManager->set_vertex_distribution_width(100e-4,100e-4,8,0);
+    }
+  }
+  // register all input generators with Fun4All
+  InputRegister();
+
+  // set up production relatedstuff
+  Enable::PRODUCTION = true;
+
+
+  SyncReco *sync = new SyncReco();
+  se->registerSubsystem(sync);
+
+  //======================
+  // Write the DST
+  //======================
+
+  Enable::DSTOUT = true;
+
+  DstOut::OutputDir = outdir;
+  DstOut::OutputFile = outputFile;
+
+  //======================
+  // What to run
+  //======================
+  // Global options (enabled for all enables subsystems - if implemented)
+  //  Enable::ABSORBER = true;
+  //  Enable::OVERLAPCHECK = true;
+  //  Enable::VERBOSITY = 1;
+
+  Enable::BBC = true;
+//  Enable::BBCFAKE = true; // Smeared vtx and t0, use if you don't want real BBC in simulation
+
+  Enable::PIPE = true;
+//  Enable::PIPE_ABSORBER = true;
+
+  // central tracking
+  Enable::MVTX = true;
+//  Enable::MVTX_CELL = Enable::MVTX && true;
+  Enable::MVTX_CLUSTER = Enable::MVTX_CELL && true;
+
+  Enable::INTT = true;
+//  Enable::INTT_CELL = Enable::INTT && true;
+  Enable::INTT_CLUSTER = Enable::INTT_CELL && true;
+
+  Enable::TPC = true;
+//  Enable::TPC_ABSORBER = true;
+//  Enable::TPC_CELL = Enable::TPC && true;
+  Enable::TPC_CLUSTER = Enable::TPC_CELL && true;
+
+  Enable::MICROMEGAS = true;
+//  Enable::MICROMEGAS_CELL = Enable::MICROMEGAS && true;
+  Enable::MICROMEGAS_CLUSTER = Enable::MICROMEGAS_CELL && true;
+
+//  Enable::TRACKING_TRACK = true;
+  Enable::TRACKING_EVAL = Enable::TRACKING_TRACK && true;
+
+  Enable::CEMC = true;
+//  Enable::CEMC_ABSORBER = true;
+//  Enable::CEMC_CELL = Enable::CEMC && true;
+  Enable::CEMC_TOWER = Enable::CEMC_CELL && true;
+  Enable::CEMC_CLUSTER = Enable::CEMC_TOWER && true;
+  Enable::CEMC_EVAL = Enable::CEMC_CLUSTER && true;
+
+  Enable::HCALIN = true;
+//  Enable::HCALIN_ABSORBER = true;
+//  Enable::HCALIN_CELL = Enable::HCALIN && true;
+  Enable::HCALIN_TOWER = Enable::HCALIN_CELL && true;
+  Enable::HCALIN_CLUSTER = Enable::HCALIN_TOWER && true;
+  Enable::HCALIN_EVAL = Enable::HCALIN_CLUSTER && true;
+
+  Enable::MAGNET = true;
+//  Enable::MAGNET_ABSORBER = true;
+
+  Enable::HCALOUT = true;
+//  Enable::HCALOUT_ABSORBER = true;
+//  Enable::HCALOUT_CELL = Enable::HCALOUT && true;
+  Enable::HCALOUT_TOWER = Enable::HCALOUT_CELL && true;
+  Enable::HCALOUT_CLUSTER = Enable::HCALOUT_TOWER && true;
+  Enable::HCALOUT_EVAL = Enable::HCALOUT_CLUSTER && true;
+
+  Enable::EPD = true;
+
+  //! forward flux return plug door. Out of acceptance and off by default.
+  Enable::PLUGDOOR = true;
+//  Enable::PLUGDOOR_ABSORBER = true;
+
+//  Enable::GLOBAL_RECO = true;
+  //  Enable::GLOBAL_FASTSIM = true;
+
+  // new settings using Enable namespace in GlobalVariables.C
+  Enable::BLACKHOLE = true;
+  //Enable::BLACKHOLE_SAVEHITS = false; // turn off saving of bh hits
+  //BlackHoleGeometry::visible = true;
+
+  // run user provided code (from local G4_User.C)
+  //Enable::USER = true;
+
+  //---------------
+  // World Settings
+  //---------------
+  G4WORLD::PhysicsList = "FTFP_BERT_HP"; //FTFP_BERT_HP best for calo
+  //  G4WORLD::WorldMaterial = "G4_AIR"; // set to G4_GALACTIC for material scans
+
+  //---------------
+  // Magnet Settings
+  //---------------
+
+  //  const string magfield = "1.5"; // alternatively to specify a constant magnetic field, give a float number, which will be translated to solenoidal field in T, if string use as fieldmap name (including path)
+  //  G4MAGNET::magfield = string(getenv("CALIBRATIONROOT")) + string("/Field/Map/sPHENIX.2d.root");  // default map from the calibration database
+  G4MAGNET::magfield_rescale = -1.4 / 1.5;  // make consistent with expected Babar field strength of 1.4T
+
+
+  // Initialize the selected subsystems
+  G4Init();
+
+  //---------------------
+  // GEANT4 Detector description
+  //---------------------
+  if (!Input::READHITS)
+  {
+    G4Setup();
+  }
+
+  string outputroot = outputFile;
+  string remove_this = ".root";
+  size_t pos = outputroot.find(remove_this);
+  if (pos != string::npos)
+  {
+    outputroot.erase(pos, remove_this.length());
+  }
+
+  //--------------
+  // Set up Input Managers
+  //--------------
+
+  InputManagers();
+
+  if (Enable::PRODUCTION)
+  {
+    Production_CreateOutputDir();
+  }
+
+  if (Enable::DSTOUT)
+  {
+    string FullOutFile = DstOut::OutputDir + "/" + DstOut::OutputFile;
+    Fun4AllDstOutputManager *out = new Fun4AllDstOutputManager("DSTOUT", FullOutFile);
+    if (Enable::DSTOUT_COMPRESS) DstCompress(out);
+    se->registerOutputManager(out);
+  }
+  //-----------------
+  // Event processing
+  //-----------------
+
+  // if we use a negative number of events we go back to the command line here
+  if (nEvents < 0)
+  {
+    return 0;
+  }
+  // if we run the particle generator and use 0 it'll run forever
+  if (nEvents == 0 && !Input::HEPMC && !Input::READHITS)
+  {
+    cout << "using 0 for number of events is a bad idea when using particle generators" << endl;
+    cout << "it will run forever, so I just return without running anything" << endl;
+    return 0;
+  }
+
+  se->skip(skip);
+  se->run(nEvents);
+
+  //-----
+  // Exit
+  //-----
+
+  se->End();
+  std::cout << "All done" << std::endl;
+  delete se;
+  if (Enable::PRODUCTION)
+  {
+    Production_MoveOutput();
+  }
+
+  gSystem->Exit(0);
+  return 0;
+}
+#endif
