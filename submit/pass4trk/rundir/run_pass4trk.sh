6ba7abf6b133e548c4e3ba1e06e5c702142f660a
sPHENIX Production Account <pinkenburg@bnl.gov>
2020-11-30 15:22:53 -0500

    Add pass4 tracking

---
+++ submit/pass4trk/rundir/run_pass4trk.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+source /opt/sphenix/core/bin/sphenix_setup.sh -n mdc1
+# arguments 
+# $1: number of events
+# $2: calo g4hits input file
+# $3: output file
+# $4: output dir
+
+echo 'here comes your environment'
+printenv
+echo arg1 \(events\) : $1
+echo arg2 \(calo g4hits file\): $2
+echo arg3 \(output file\): $3
+echo arg4 \(output dir\): $4
+echo running root.exe -q -b Fun4All_G4_Tracking.C\($1,\"$2\",\"$3\",\"$4\"\)
+root.exe -q -b  Fun4All_G4_Tracking.C\($1,\"$2\",\"$3\",\"$4\"\)
+echo "script done"
