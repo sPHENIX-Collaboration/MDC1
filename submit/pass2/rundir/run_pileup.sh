f9ad21dd5c6ea63d376bb549bc87766b5ee14843
sPHENIX Production Account <pinkenburg@bnl.gov>
2020-12-12 18:33:07 -0500

    create and add background list - 3 files startign with segment+1, use filecatalog, use mdc1.2 build

--- submit/pass2/rundir/run_pileup.sh
+++ submit/pass2/rundir/run_pileup.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/bash
-source /opt/sphenix/core/bin/sphenix_setup.sh -n mdc1
+source /opt/sphenix/core/bin/sphenix_setup.sh -n mdc1.2
 
 # arguments 
-# $1: number of input events
-# $2: number of output events
-# $3: input file
+# $1: number of output events
+# $2: input file
+# $3: background listfile
 # $4: output directory
 
 echo 'here comes your environment'
 printenv
-echo arg1 \(input events\) : $1
-echo arg2 \(output events\) : $2
-echo arg3 \(input file\): $3
+echo arg1 \(output events\) : $1
+echo arg2 \(input file\): $2
+echo arg3 \(background listfile\): $3
 echo arg4 \(output dir\): $4
-echo running root.exe -q -b Fun4All_G4_Pileup.C\($1,$2,\"$3\",\"$4\"\)
-root.exe -q -b  Fun4All_G4_Pileup.C\($1,$2,\"$3\",\"$4\"\)
+echo running root.exe -q -b Fun4All_G4_Pileup.C\($1,\"$2\",\"$3\",\"$4\"\)
+root.exe -q -b  Fun4All_G4_Pileup.C\($1,\"$2\",\"$3\",\"$4\"\)
 echo "script done"
