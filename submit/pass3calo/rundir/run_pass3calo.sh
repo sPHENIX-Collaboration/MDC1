928441543b69dd1aefded59dee1bc9de549669fb
sPHENIX Production Account <pinkenburg@bnl.gov>
2020-12-14 13:28:21 -0500

    set HOME and echo command line

--- submit/pass3calo/rundir/run_pass3calo.sh
+++ submit/pass3calo/rundir/run_pass3calo.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/bash
+export HOME=/sphenix/u/${LOGNAME}
 source /opt/sphenix/core/bin/sphenix_setup.sh -n mdc1.2
+
+echo running: run_pass3calo.sh $*
+
 # arguments 
 # $1: number of events
 # $2: calo g4hits input file
