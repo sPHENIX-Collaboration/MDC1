6897e72c109ef770ef2b28122c38c0a6b06d9ebc
sPHENIX Production Account <pinkenburg@bnl.gov>
2020-12-23 17:19:05 -0500

    cron to save condor logs daily at 7.17 am

---
+++ cronjobs/rsync_condorlogs.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my $submitdir = "/sphenix/u/sphnxpro/MDC1/submit";
+my %condorlogs = ();
+my @fmrange = ();
+push(@fmrange,"fm_0_488");
+push(@fmrange,"fm_0_12");
+
+my @passes = ();
+push(@passes,"pass1");
+push(@passes,"pass2");
+push(@passes,"pass3trk");
+push(@passes,"pass3calo");
+push(@passes,"pass4trk");
+
+foreach my $fm (sort @fmrange)
+{
+    foreach my $pass (sort @passes)
+    {
+	$condorlogs{sprintf("/tmp/%s/%s",$fm,$pass)} = sprintf("%s/%s/%s/condor/log",$submitdir,$fm,$pass);
+
+    }
+}
+
+foreach my $condorlogdir (sort keys %condorlogs)
+{
+    if (-d $condorlogdir && -d $condorlogs{$condorlogdir})
+    {
+	my $rsynccmd = sprintf("rsync -av %s %s",$condorlogdir, $condorlogs{$condorlogdir});
+	print "cmd: $rsynccmd\n";
+	system($rsynccmd);
+    }
+}
