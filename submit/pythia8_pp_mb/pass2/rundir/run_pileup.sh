51d659c112714e32d4711db779d2ec9585ec0972
sPHENIX Production Account <pinkenburg@bnl.gov>
2021-01-05 15:26:14 -0500

    macros to run pp pileup

---
+++ submit/pythia8_pp_mb/pass2/rundir/run_pileup.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+export HOME=/sphenix/u/${LOGNAME}
+source /opt/sphenix/core/bin/sphenix_setup.sh -n mdc1.2
+
+echo running: run_pileup.sh $*
+
+# arguments 
+# $1: number of output events
+# $2: input file
+# $3: background listfile
+# $4: output directory
+
+echo 'here comes your environment'
+printenv
+echo arg1 \(output events\) : $1
+echo arg2 \(input file\): $2
+echo arg3 \(background listfile\): $3
+echo arg4 \(output dir\): $4
+echo running root.exe -q -b Fun4All_G4_Pileup_pp.C\($1,\"$2\",\"$3\",\"$4\"\)
+root.exe -q -b  Fun4All_G4_Pileup_pp.C\($1,\"$2\",\"$3\",\"$4\"\)
+echo "script done"
