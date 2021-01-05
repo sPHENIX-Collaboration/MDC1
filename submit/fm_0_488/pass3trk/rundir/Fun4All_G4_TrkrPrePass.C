e6ee00c945f33166d7d808135215f6c0de419d71
sPHENIX Production Account <pinkenburg@bnl.gov>
2021-01-05 00:05:52 -0500

    Add pass3 trk

---
+++ submit/fm_0_488/pass3trk/rundir/Fun4All_G4_TrkrPrePass.C
@@ -0,0 +1,337 @@
+#ifndef MACRO_FUN4ALLG4SPHENIX_C
+#define MACRO_FUN4ALLG4SPHENIX_C
+
+#include <GlobalVariables.C>
+
+#include <DisplayOn.C>
+#include <G4Setup_sPHENIX.C>
+#include <G4_Bbc.C>
+#include <G4_CaloTrigger.C>
+#include <G4_DSTReader.C>
+#include <G4_Global.C>
+#include <G4_HIJetReco.C>
+#include <G4_Input_PrePass.C>
+#include <G4_Jets.C>
+#include <G4_ParticleFlow.C>
+#include <G4_Production.C>
+#include <G4_TopoClusterReco.C>
+#include <G4_Tracking_PrePass.C>
+#include <G4_User.C>
+
+#include <fun4all/Fun4AllDstOutputManager.h>
+#include <fun4all/Fun4AllOutputManager.h>
+#include <fun4all/Fun4AllServer.h>
+
+#include <phool/PHRandomSeed.h>
+#include <phool/recoConsts.h>
+
+R__LOAD_LIBRARY(libfun4all.so)
+
+// For HepMC Hijing
+// try inputFile = /sphenix/sim/sim01/sphnxpro/sHijing_HepMC/sHijing_0-12fm.dat
+
+int Fun4All_G4_TrkrPrePass(
+    const int nEvents = 1,
+    const string &inputFile0 = "https://www.phenix.bnl.gov/WWW/publish/phnxbld/sPHENIX/files/sPHENIX_G4Hits_sHijing_9-11fm_00000_00010.root",
+    const string &inputFile1 = "https://www.phenix.bnl.gov/WWW/publish/phnxbld/sPHENIX/files/sPHENIX_G4Hits_sHijing_9-11fm_00000_00010.root",
+    const string &outputFile = "G4sPHENIX_TrkrCluster.root",
+    const string &outdir = ".")
+{
+  Fun4AllServer *se = Fun4AllServer::instance();
+  se->Verbosity(1);
+
+  //Opt to print all random seed used for debugging reproducibility. Comment out to reduce stdout prints.
+  PHRandomSeed::Verbosity(1);
+
+  // just if we set some flags somewhere in this macro
+  recoConsts *rc = recoConsts::instance();
+  // By default every random number generator uses
+  // PHRandomSeed() which reads /dev/urandom to get its seed
+  // if the RANDOMSEED flag is set its value is taken as seed
+  // You can either set this to a random value using PHRandomSeed()
+  // which will make all seeds identical (not sure what the point of
+  // this would be:
+  //  rc->set_IntFlag("RANDOMSEED",PHRandomSeed());
+  // or set it to a fixed value so you can debug your code
+  //  rc->set_IntFlag("RANDOMSEED", 12345);
+
+  //===============
+  // Input options
+  //===============
+  // verbosity setting (applies to all input managers)
+  Input::VERBOSITY = 1;
+  // First enable the input generators
+  // Either:
+  // read previously generated g4-hits files, in this case it opens a DST and skips
+  // the simulations step completely. The G4Setup macro is only loaded to get information
+  // about the number of layers used for the cell reco code
+  Input::READHITS = true;
+  INPUTREADHITS::filename[0] = inputFile0;
+  INPUTREADHITS::filename[1] = inputFile1;
+
+  //-----------------
+  // Initialize the selected Input/Event generation
+  //-----------------
+  // This creates the input generator(s)
+  InputInit();
+
+  // register all input generators with Fun4All
+  InputRegister();
+
+  // set up production relatedstuff
+   Enable::PRODUCTION = true;
+
+  //======================
+  // Write the DST
+  //======================
+
+  Enable::DSTOUT = true;
+  Enable::DSTOUT_COMPRESS = false;
+  DstOut::OutputDir = outdir;
+  DstOut::OutputFile = outputFile;
+
+  //Option to convert DST to human command readable TTree for quick poke around the outputs
+  //  Enable::DSTREADER = true;
+
+  // turn the display on (default off)
+  Enable::DISPLAY = false;
+
+  //======================
+  // What to run
+  //======================
+  // Global options (enabled for all enables subsystems - if implemented)
+  //  Enable::ABSORBER = true;
+  //  Enable::OVERLAPCHECK = true;
+  //  Enable::VERBOSITY = 1;
+
+  // central tracking
+  Enable::MVTX = true;
+  Enable::MVTX_CELL = Enable::MVTX && true;
+  Enable::MVTX_CLUSTER = Enable::MVTX_CELL && true;
+
+  Enable::INTT = true;
+  Enable::INTT_CELL = Enable::INTT && true;
+  Enable::INTT_CLUSTER = Enable::INTT_CELL && true;
+
+  Enable::TPC = true;
+  Enable::TPC_ABSORBER = true;
+  Enable::TPC_CELL = Enable::TPC && true;
+  Enable::TPC_CLUSTER = Enable::TPC_CELL && true;
+
+  Enable::MICROMEGAS = true;
+  Enable::MICROMEGAS_CELL = Enable::MICROMEGAS && true;
+  Enable::MICROMEGAS_CLUSTER = Enable::MICROMEGAS_CELL && true;
+
+  Enable::TRACKING_TRACK = true;
+//  Enable::TRACKING_EVAL = Enable::TRACKING_TRACK && true;
+
+  //---------------
+  // Magnet Settings
+  //---------------
+
+  //  const string magfield = "1.5"; // alternatively to specify a constant magnetic field, give a float number, which will be translated to solenoidal field in T, if string use as fieldmap name (including path)
+  //  G4MAGNET::magfield = string(getenv("CALIBRATIONROOT")) + string("/Field/Map/sPHENIX.2d.root");  // default map from the calibration database
+  G4MAGNET::magfield_rescale = -1.4 / 1.5;  // make consistent with expected Babar field strength of 1.4T
+
+  //---------------
+  // Pythia Decayer
+  //---------------
+  // list of decay types in
+  // $OFFLINE_MAIN/include/g4decayer/EDecayType.hh
+  // default is All:
+  // G4P6DECAYER::decayType = EDecayType::kAll;
+
+  // Initialize the selected subsystems
+  G4Init();
+
+  //---------------------
+  // GEANT4 Detector description
+  //---------------------
+  if (!Input::READHITS)
+  {
+    G4Setup();
+  }
+
+  //------------------
+  // Detector Division
+  //------------------
+
+  if (Enable::BBC || Enable::BBCFAKE) Bbc_Reco();
+
+  if (Enable::MVTX_CELL) Mvtx_Cells();
+  if (Enable::INTT_CELL) Intt_Cells();
+  if (Enable::TPC_CELL) TPC_Cells();
+  if (Enable::MICROMEGAS_CELL) Micromegas_Cells();
+
+  if (Enable::CEMC_CELL) CEMC_Cells();
+
+  if (Enable::HCALIN_CELL) HCALInner_Cells();
+
+  if (Enable::HCALOUT_CELL) HCALOuter_Cells();
+
+  if (Enable::FEMC_CELL) FEMC_Cells();
+
+  //-----------------------------
+  // CEMC towering and clustering
+  //-----------------------------
+
+  if (Enable::CEMC_TOWER) CEMC_Towers();
+  if (Enable::CEMC_CLUSTER) CEMC_Clusters();
+
+  //-----------------------------
+  // HCAL towering and clustering
+  //-----------------------------
+
+  if (Enable::HCALIN_TOWER) HCALInner_Towers();
+  if (Enable::HCALIN_CLUSTER) HCALInner_Clusters();
+
+  if (Enable::HCALOUT_TOWER) HCALOuter_Towers();
+  if (Enable::HCALOUT_CLUSTER) HCALOuter_Clusters();
+
+  // if enabled, do topoClustering early, upstream of any possible jet reconstruction
+  if (Enable::TOPOCLUSTER) TopoClusterReco();
+
+  if (Enable::FEMC_TOWER) FEMC_Towers();
+  if (Enable::FEMC_CLUSTER) FEMC_Clusters();
+
+  if (Enable::DSTOUT_COMPRESS) ShowerCompress();
+
+  //--------------
+  // SVTX tracking
+  //--------------
+  if (Enable::MVTX_CLUSTER) Mvtx_Clustering();
+  if (Enable::INTT_CLUSTER) Intt_Clustering();
+  if (Enable::TPC_CLUSTER) TPC_Clustering();
+  if (Enable::MICROMEGAS_CLUSTER) Micromegas_Clustering();
+
+  if (Enable::TRACKING_TRACK)
+  {
+    TrackingInit();
+    Tracking_Reco();
+  }
+  //-----------------
+  // Global Vertexing
+  //-----------------
+
+  if (Enable::GLOBAL_RECO && Enable::GLOBAL_FASTSIM)
+  {
+    cout << "You can only enable Enable::GLOBAL_RECO or Enable::GLOBAL_FASTSIM, not both" << endl;
+    gSystem->Exit(1);
+  }
+  if (Enable::GLOBAL_RECO)
+  {
+    Global_Reco();
+  }
+  else if (Enable::GLOBAL_FASTSIM)
+  {
+    Global_FastSim();
+  }
+
+  //-----------------
+  // Calo Trigger Simulation
+  //-----------------
+
+  if (Enable::CALOTRIGGER)
+  {
+    CaloTrigger_Sim();
+  }
+
+  //---------
+  // Jet reco
+  //---------
+
+  if (Enable::JETS) Jet_Reco();
+  if (Enable::HIJETS) HIJetReco();
+
+  if (Enable::PARTICLEFLOW) ParticleFlow();
+
+  //----------------------
+  // Simulation evaluation
+  //----------------------
+  string outputroot = outputFile;
+  string remove_this = ".root";
+  size_t pos = outputroot.find(remove_this);
+  if (pos != string::npos)
+  {
+    outputroot.erase(pos, remove_this.length());
+  }
+
+  //--------------
+  // Set up Input Managers
+  //--------------
+
+  InputManagers();
+
+  if (Enable::PRODUCTION)
+  {
+    Production_CreateOutputDir();
+  }
+
+  if (Enable::DSTOUT)
+  {
+    string FullOutFile = DstOut::OutputDir + "/" + DstOut::OutputFile;
+    Fun4AllDstOutputManager *out = new Fun4AllDstOutputManager("DSTOUT", FullOutFile);
+out->AddNode("Sync");
+out->AddNode("EventHeader");
+out->AddNode("TRKR_HITSET");
+out->AddNode("TRKR_HITTRUTHASSOC");
+out->AddNode("TRKR_CLUSTER");
+out->AddNode("TRKR_CLUSTERHITASSOC");
+out->AddNode("SvtxVertexMap");
+out->AddNode("SvtxSiliconTrackMap");
+out->AddNode("AssocInfoContainer");
+    if (Enable::DSTOUT_COMPRESS) DstCompress(out);
+    se->registerOutputManager(out);
+  }
+  //-----------------
+  // Event processing
+  //-----------------
+  if (Enable::DISPLAY)
+  {
+    DisplayOn();
+
+    gROOT->ProcessLine("Fun4AllServer *se = Fun4AllServer::instance();");
+    gROOT->ProcessLine("PHG4Reco *g4 = (PHG4Reco *) se->getSubsysReco(\"PHG4RECO\");");
+
+    cout << "-------------------------------------------------" << endl;
+    cout << "You are in event display mode. Run one event with" << endl;
+    cout << "se->run(1)" << endl;
+    cout << "Run Geant4 command with following examples" << endl;
+    gROOT->ProcessLine("displaycmd()");
+
+    return 0;
+  }
+
+  // if we use a negative number of events we go back to the command line here
+  if (nEvents < 0)
+  {
+    return 0;
+  }
+  // if we run the particle generator and use 0 it'll run forever
+  if (nEvents == 0 && !Input::HEPMC && !Input::READHITS)
+  {
+    cout << "using 0 for number of events is a bad idea when using particle generators" << endl;
+    cout << "it will run forever, so I just return without running anything" << endl;
+    return 0;
+  }
+
+  se->run(nEvents);
+
+  //-----
+  // Exit
+  //-----
+
+  se->End();
+  se->PrintTimer();
+  se->PrintMemoryTracker();
+  std::cout << "All done" << std::endl;
+  delete se;
+  if (Enable::PRODUCTION)
+  {
+    Production_MoveOutput();
+  }
+
+  gSystem->Exit(0);
+  return 0;
+}
+#endif
---
+++ submit/fm_0_488/pass3trk/rundir/run_pass3trk.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+
+export HOME=/sphenix/u/${LOGNAME}
+
+source /opt/sphenix/core/bin/sphenix_setup.sh -n mdc1.3
+
+echo running: run_pass3trk.sh $*
+
+# arguments 
+# $1: number of events
+# $2: track g4hits input file
+# $3: truth g4hits input file
+# $4: output file
+# $5: output dir
+
+echo 'here comes your environment'
+printenv
+echo arg1 \(events\) : $1
+echo arg2 \(track g4hits file\): $2
+echo arg3 \(truth g4hits file\): $3
+echo arg4 \(output file\): $4
+echo arg5 \(output dir\): $5
+echo running root.exe -q -b Fun4All_G4_TrkrPrePass.C\($1,\"$2\",\"$3\",\"$4\",\"$5\"\)
+root.exe -q -b  Fun4All_G4_TrkrPrePass.C\($1,\"$2\",\"$3\",\"$4\",\"$5\"\)
+echo "script done"
