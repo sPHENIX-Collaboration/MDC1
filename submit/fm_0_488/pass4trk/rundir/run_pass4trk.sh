8723bfd365d08ae692a067a73de1eb0412a0d767
sPHENIX Production Account <pinkenburg@bnl.gov>
2021-01-06 15:15:54 -0500

    Add pass4trk for 0-4.88 fm

---
+++ submit/fm_0_488/pass4trk/rundir/run_pass4trk.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+export HOME=/sphenix/u/${LOGNAME}
+source /opt/sphenix/core/bin/sphenix_setup.sh -n mdc1.3
+
+echo running: run_pass4trk.sh $*
+
+# arguments 
+# $1: number of events
+# $2: calo g4hits input file
+# $3: output file
+# $4: output dir
+
+echo 'here comes your environment'
+printenv
+echo arg1 \(events\) : $1
+echo arg2 \(calo g4hits file\): $2
+echo arg3 \(output file\): $3
+echo arg4 \(output dir\): $4
+echo running root.exe -q -b Fun4All_G4_Tracking.C\($1,\"$2\",\"$3\",\"$4\"\)
+root.exe -q -b  Fun4All_G4_Tracking.C\($1,\"$2\",\"$3\",\"$4\"\)
+echo "script done"
