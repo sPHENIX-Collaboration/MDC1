8348cc9f6098f16ed0df5c996d05fe040b09326c
sPHENIX Production Account <pinkenburg@bnl.gov>
2021-01-13 10:01:06 -0500

    ad macro to read files for integrity check

---
+++ submit/filecatalog/TestRead.C
@@ -0,0 +1,16 @@
+#include <fun4all/Fun4AllServer.h>
+#include <fun4all/Fun4AllDstInputManager.h>
+R__LOAD_LIBRARY(libfun4all.so)
+
+void TestRead(const std::string &name)
+{
+gSystem->Load("libg4dst.so");
+Fun4AllServer *se = Fun4AllServer::instance();
+Fun4AllDstInputManager *in = new Fun4AllDstInputManager("DSTin");
+in->fileopen(name);
+se->registerInputManager(in);
+se->run();
+se->End();
+delete se;
+gSystem->Exit(0);
+}
