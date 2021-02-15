1efd5ac205ad2e5433b4e30c175558b2e00515ab
Chris Pinkenburg <pinkenburg@bnl.gov>
2021-02-15 11:52:43 -0500

    adjust submission for pass4 0-4.88fm

--- submit/fm_0_488/pass4trk_50kHz_0_20fm/condor/run_all.pl
+++ submit/fm_0_488/pass4trk_50kHz_0_20fm/condor/run_all.pl
@@ -21,6 +21,14 @@ if ($#ARGV < 0)
     exit(1);
 }
 
+my $hostname = `hostname`;
+chomp $hostname;
+if ($hostname !~ /phnxsub/)
+{
+    print "submit only from phnxsub01 or phnxsub02\n";
+    exit(1);
+}
+
 my $maxsubmit = $ARGV[0];
 if (! -f "outdir.txt")
 {
@@ -31,29 +39,54 @@ my $outdir = `cat outdir.txt`;
 chomp $outdir;
 mkpath($outdir);
 
-my $nsubmit = 0;
+
+my %trkhash = ();
+my %truthhash = ();
+
 my $dbh = DBI->connect("dbi:ODBC:FileCatalog","phnxrc") || die $DBI::error;
 $dbh->{LongReadLen}=2000; # full file paths need to fit in here
-my $getfiles = $dbh->prepare("select filename from datasets where dsttype = 'DST_TRKR_CLUSTER' and filename like '%sHijing_0_488fm%' order by filename") || die $DBI::error;
+my $getfiles = $dbh->prepare("select filename,segment from datasets where dsttype = 'DST_TRKR_CLUSTER' and filename like '%sHijing_0_488fm_50kHz_bkg_0_20fm%' order by filename") || die $DBI::error;
 my $chkfile = $dbh->prepare("select lfn from files where lfn=?") || die $DBI::error;
+my $gettruthfiles = $dbh->prepare("select filename,segment from datasets where dsttype = 'DST_TRUTH_G4HIT' and filename like '%sHijing_0_488fm_50kHz_bkg_0_20fm%'");
+
 my $nsubmit = 0;
 $getfiles->execute() || die $DBI::error;
 while (my @res = $getfiles->fetchrow_array())
 {
-    my $lfn = $res[0];
+    $trkhash{sprintf("%05d",$res[1])} = $res[0];
+}
+$getfiles->finish();
+$gettruthfiles->execute() || die $DBI::error;
+my $ntruth = $gettruthfiles->rows;
+while (my @res = $gettruthfiles->fetchrow_array())
+{
+    $truthhash{sprintf("%05d",$res[1])} = $res[0];
+}
+foreach my $segment (sort keys %trkhash)
+{
+    if (! exists $truthhash{$segment})
+    {
+	next;
+    }
+    my $lfn = $trkhash{$segment};
     print "found $lfn\n";
     if ($lfn =~ /(\S+)-(\d+)-(\d+).*\..*/ )
     {
 	my $runnumber = int($2);
 	my $segment = int($3);
-	my $outfilename = sprintf("DST_TRACKS_sHijing_0_488fm-%010d-%05d.root",$runnumber,$segment);
+	my $outfilename = sprintf("DST_TRACKS_sHijing_0_488fm_50kHz_bkg_0_20fm-%010d-%05d.root",$runnumber,$segment);
+	$chkfile->execute($outfilename);
+	if ($chkfile->rows > 0)
+	{
+	    next;
+	}
 
 	my $tstflag="";
 	if (defined $test)
 	{
 	    $tstflag="--test";
 	}
-	my $subcmd = sprintf("perl run_condor.pl %d %s %s %s %d %d %s", $outevents, $lfn, $outfilename, $outdir, $runnumber, $segment, $tstflag);
+	my $subcmd = sprintf("perl run_condor.pl %d %s %s %s %s %d %d %s", $outevents, $truthhash{sprintf("%05d",$segment)}, $lfn, $outfilename, $outdir, $runnumber, $segment, $tstflag);
 	print "cmd: $subcmd\n";
 	system($subcmd);
 	my $exit_value  = $? >> 8;
@@ -76,3 +109,6 @@ while (my @res = $getfiles->fetchrow_array())
 	}
     }
 }
+
+$chkfile->finish();
+$dbh->disconnect;
--- submit/fm_0_488/pass4trk_50kHz_0_20fm/condor/run_condor.pl
+++ submit/fm_0_488/pass4trk_50kHz_0_20fm/condor/run_condor.pl
@@ -28,7 +28,7 @@ my $sequence = $ARGV[5];
 my $suffix = sprintf("%010d-%05d",$runnumber,$sequence);
 my $logdir = sprintf("%s/log",$localdir);
 mkpath($logdir);
-my $condorlogdir = sprintf("/tmp/fm_0_488/pass4trk");
+my $condorlogdir = sprintf("/tmp/fm_0_488/pass4trk_50kHz_0_20fm");
 mkpath($condorlogdir);
 my $jobfile = sprintf("%s/condor-%s.job",$logdir,$suffix);
 if (-f $jobfile)
@@ -47,7 +47,7 @@ print "job: $jobfile\n";
 open(F,">$jobfile");
 print F "Universe 	= vanilla\n";
 print F "Executable 	= $executable\n";
-print F "Arguments       = \"$nevents $infile $dstoutfile $dstoutdir\"\n";
+print F "Arguments       = \"$nevents $infile0 $infile1 $dstoutfile $dstoutdir\"\n";
 print F "Output  	= $outfile\n";
 print F "Error 		= $errfile\n";
 print F "Log  		= $condorlogfile\n";
