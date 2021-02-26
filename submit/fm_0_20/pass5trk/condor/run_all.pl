0932356cf84968e123c23146d491131c3eddd9f8
Chris Pinkenburg <pinkenburg@bnl.gov>
2021-02-26 14:18:29 -0500

    add pass5trk tracking only pass

---
+++ submit/fm_0_20/pass5trk/condor/run_all.pl
@@ -0,0 +1,114 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use File::Path;
+use File::Basename;
+use Getopt::Long;
+use DBI;
+
+
+my $outevents = 0;
+my $test;
+my $incremental;
+GetOptions("test"=>\$test, "increment"=>\$incremental);
+if ($#ARGV < 0)
+{
+    print "usage: run_all.pl <number of jobs>\n";
+    print "parameters:\n";
+    print "--increment : submit jobs while processing running\n";
+    print "--test : dryrun - create jobfiles\n";
+    exit(1);
+}
+
+my $hostname = `hostname`;
+chomp $hostname;
+if ($hostname !~ /phnxsub/)
+{
+    print "submit only from phnxsub01 or phnxsub02\n";
+    exit(1);
+}
+
+my $maxsubmit = $ARGV[0];
+if (! -f "outdir.txt")
+{
+    print "could not find outdir.txt\n";
+    exit(1);
+}
+my $outdir = `cat outdir.txt`;
+chomp $outdir;
+mkpath($outdir);
+
+
+my %trkhash = ();
+my %truthhash = ();
+
+my $dbh = DBI->connect("dbi:ODBC:FileCatalog","phnxrc") || die $DBI::error;
+$dbh->{LongReadLen}=2000; # full file paths need to fit in here
+my $getfiles = $dbh->prepare("select filename,segment from datasets where dsttype = 'DST_TRKR_CLUSTER' and filename like '%sHijing_0_20fm_50kHz_bkg_0_20fm%' order by filename") || die $DBI::error;
+my $chkfile = $dbh->prepare("select lfn from files where lfn=?") || die $DBI::error;
+my $gettruthfiles = $dbh->prepare("select filename,segment from datasets where dsttype = 'DST_TRUTH_G4HIT' and filename like '%sHijing_0_20fm_50kHz_bkg_0_20fm%'");
+
+my $nsubmit = 0;
+$getfiles->execute() || die $DBI::error;
+while (my @res = $getfiles->fetchrow_array())
+{
+    $trkhash{sprintf("%05d",$res[1])} = $res[0];
+}
+$getfiles->finish();
+$gettruthfiles->execute() || die $DBI::error;
+my $ntruth = $gettruthfiles->rows;
+while (my @res = $gettruthfiles->fetchrow_array())
+{
+    $truthhash{sprintf("%05d",$res[1])} = $res[0];
+}
+foreach my $segment (sort keys %trkhash)
+{
+    if (! exists $truthhash{$segment})
+    {
+	next;
+    }
+    my $lfn = $trkhash{$segment};
+    print "found $lfn\n";
+    if ($lfn =~ /(\S+)-(\d+)-(\d+).*\..*/ )
+    {
+	my $runnumber = int($2);
+	my $segment = int($3);
+	my $outfilename = sprintf("DST_RECOTRACKS_sHijing_0_20fm_50kHz_bkg_0_20fm-%010d-%05d.root",$runnumber,$segment);
+	$chkfile->execute($outfilename);
+	if ($chkfile->rows > 0)
+	{
+	    next;
+	}
+
+	my $tstflag="";
+	if (defined $test)
+	{
+	    $tstflag="--test";
+	}
+	my $subcmd = sprintf("perl run_condor.pl %d %s %s %s %s %d %d %s", $outevents, $truthhash{sprintf("%05d",$segment)}, $lfn, $outfilename, $outdir, $runnumber, $segment, $tstflag);
+	print "cmd: $subcmd\n";
+	system($subcmd);
+	my $exit_value  = $? >> 8;
+	if ($exit_value != 0)
+	{
+	    if (! defined $incremental)
+	    {
+		print "error from run_condor.pl\n";
+		exit($exit_value);
+	    }
+	}
+	else
+	{
+	    $nsubmit++;
+	}
+	if ($nsubmit >= $maxsubmit)
+	{
+	    print "maximum number of submissions reached, exiting\n";
+	    exit(0);
+	}
+    }
+}
+
+$chkfile->finish();
+$dbh->disconnect;
