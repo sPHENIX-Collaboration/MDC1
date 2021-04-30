bf4d3bd888e3c9c4a13536b088b6ffd5ac2d8925
Chris Pinkenburg <pinkenburg@users.noreply.github.com>
2021-04-29 20:55:11 -0400

    Update Fun4All_G4_HF_pp_signal.C

--- submit/HF_pp200_signal/rundir/Fun4All_G4_HF_pp_signal.C
+++ submit/HF_pp200_signal/rundir/Fun4All_G4_HF_pp_signal.C
@@ -38,7 +38,7 @@ R__LOAD_LIBRARY(libfun4all.so)
 
 int Fun4All_G4_HF_pp_signal(
     const int nEvents = 1,
-    const string &HF_Q_filter = "Charm", // or "Bottom" or "minBias"
+    const string &HF_Q_filter = "Charm", // or "Bottom" or "MinBias"
     const string &outputFile = "G4sPHENIX.root",
     const string &embed_input_file = "https://www.phenix.bnl.gov/WWW/publish/phnxbld/sPHENIX/files/sPHENIX_G4Hits_sHijing_9-11fm_00000_00010.root",
     const int skip = 0,
