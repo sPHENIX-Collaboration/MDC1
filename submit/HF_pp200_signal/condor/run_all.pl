6f6001693e9fcb6f5ebbf8ebc6b85d9c2f503c43
sPHENIX Production Account <pinkenburg@bnl.gov>
2021-05-20 14:49:00 -0400

    fix typo

--- submit/HF_pp200_signal/condor/run_all.pl
+++ submit/HF_pp200_signal/condor/run_all.pl
@@ -10,7 +10,7 @@ my $incremental;
 GetOptions("test"=>\$test, "increment"=>\$incremental);
 if ($#ARGV < 1)
 {
-    print "usage: run_all.pl <number of jobs> <\"Charm\", \"Bottom\ or \"MinBias\" production>\n";
+    print "usage: run_all.pl <number of jobs> <\"Charm\", \"Bottom\" or \"MinBias\" production>\n";
     print "parameters:\n";
     print "--increment : submit jobs while processing running\n";
     print "--test : dryrun - create jobfiles\n";
