c4a66362d55c58fa2570d2859604b36214ba15b9
Chris Pinkenburg <pinkenburg@bnl.gov>
2021-05-24 13:05:07 -0400

    add pass3 conversion for 0-20fm

---
+++ submit/FixDST/fm_0_20/pass3trk/rundir/run_fixdst.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/bash
+export HOME=/sphenix/u/${LOGNAME}
+source /opt/sphenix/core/bin/sphenix_setup.sh -n ana.241
+source /opt/sphenix/core/bin/setup_local.sh /opt/sphenix/core/FixDST/pass1/install
+
+echo running: run_fixdst.sh $*
+
+if [[ ! -z "$_CONDOR_SCRATCH_DIR" && -d $_CONDOR_SCRATCH_DIR ]]
+then
+    cd $_CONDOR_SCRATCH_DIR
+#    rsync -av /sphenix/u/sphnxpro/MDC1/submit/FixDST/fm_0_20/pass3trk/rundir/* .
+    rsync -av /sphenix/user/pinkenbu/MDC1/submit/FixDST/fm_0_20/pass3trk/rundir/* .
+    getinputfiles.pl $1
+    if [ $? -ne 0 ]
+    then
+	echo error from getinputfiles.pl $1, exiting
+	exit -1
+    fi
+else
+    echo condor scratch NOT set
+fi
+# arguments 
+# $1: number of events
+# $2: output file
+# $3: output dir
+
+echo 'here comes your environment'
+printenv
+echo arg1 \(input DST\) : $1
+echo running root.exe -q -b run_pass1.C\(\"$1\"\)
+root.exe -q -b run_pass1.C\(\"$1\"\)
+echo "pass1 done"
+
+if [[ ! -f "pass1out.root" ]]
+then
+ echo "pass1out.root missing"
+ exit 1
+fi
+
+source /opt/sphenix/core/bin/sphenix_setup.sh -n ana.250
+source /opt/sphenix/core/bin/setup_local.sh /opt/sphenix/core/FixDST/pass2/install
+
+echo running root.exe -q -b run_pass2.C\(\"pass1out.root\",\"$2\",\"$3\"\)
+root.exe -q -b run_pass2.C\(\"pass1out.root\",\"$2\",\"$3\"\)
+echo "pass2 done"
---
+++ submit/FixDST/fm_0_20/pass3trk/rundir/run_pass1.C
@@ -0,0 +1,40 @@
+#ifndef MACRO_RUNPASS1_C
+#define MACRO_RUNPASS1_C
+
+#include <fixdstpass1/fixdstpass1.h>
+#include <fun4all/Fun4AllServer.h>
+#include <fun4all/Fun4AllDstInputManager.h>
+#include <fun4all/Fun4AllDstOutputManager.h>
+
+R__LOAD_LIBRARY(libfun4all.so)
+R__LOAD_LIBRARY(libfixdstpass1.so)
+
+
+void run_pass1(const std::string &infile, const int evts=0)
+{
+  gSystem->Load("libg4dst.so");
+  gSystem->Load("libkfparticle_sphenix_io.so");
+  Fun4AllServer* se = Fun4AllServer::instance();
+
+  fixdstpass1 *p1 = new fixdstpass1();
+
+  se->registerSubsystem(p1);
+
+  Fun4AllInputManager *in = new Fun4AllDstInputManager("DSTin");
+  se->registerInputManager(in);
+  Fun4AllOutputManager *out = new Fun4AllDstOutputManager("DSTout","pass1out.root");
+
+  out->StripNode("AssocInfoContainer");
+  out->StripNode("TRKR_CLUSTER");
+  out->StripNode("TRKR_CLUSTERHITASSOC");
+  out->StripNode("TRKR_HITSET");
+  out->StripNode("TRKR_HITTRUTHASSOC");
+
+  se->registerOutputManager(out);
+  se->fileopen("DSTin",infile);
+  se->run(evts);
+  se->End();
+  delete se;
+}
+
+#endif
