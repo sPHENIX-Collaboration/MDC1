7048e4065b5bb27ec95667d7bafde6282220bd9d
sPHENIX Production Account <pinkenburg@bnl.gov>
2021-05-24 22:36:05 -0400

    fix benign typo

--- submit/FixDST/fm_0_20/pass3trk/condor/run_condor.pl
+++ submit/FixDST/fm_0_20/pass3trk/condor/run_condor.pl
@@ -27,7 +27,7 @@ my $sequence = $ARGV[4];
 my $suffix = sprintf("%010d-%05d",$runnumber,$sequence);
 my $logdir = sprintf("%s/log",$localdir);
 mkpath($logdir);
-my $condorlogdir = sprintf("/tmp//FixDST/fm_0_20/pass3trk");
+my $condorlogdir = sprintf("/tmp/FixDST/fm_0_20/pass3trk");
 mkpath($condorlogdir);
 my $jobfile = sprintf("%s/condor-%s.job",$logdir,$suffix);
 if (-f $jobfile)
